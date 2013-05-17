40cdfb09903587aa7831ea092f7c1864ac93d04f
David Arvelo <david@davidarvelo.com>
2013-05-17 16:44:22 -0400

    Fix documentation and formatting. Allow errors in debug mode (development) to bubble up to the console

--- app/scripts/app/core/errorlog.js
+++ app/scripts/app/core/errorlog.js
@@ -14,6 +14,33 @@ function (Backbone, $, _) {
    *   severity 5: lowest  - low importance, fix when there is time available
    */
 
+  /**
+   * Rules:
+   *  1. Assume your code will fail.
+   *  2. Log errors to the server.
+   *  3. You, not the browser, handle errors.
+   *  4. Identify where errors might occur.
+   *  5. Throw your own errors.
+   *  6. Distinguish fatal vs. non-fatal.
+   *  7. Provide a debug mode.
+   */
+
+
+  /**
+   * Debug mode means:
+   *  - try-catch should rethrow error
+   *  - window.onerror should return false meaning..
+   *  - allow the browser to handle the error
+   */
+
+  /**
+   * Remember:
+   *  - Status code 304 is also an "error" that should be handled wherever expecting 200.
+   *  - Error messages can be returned from the server with 500 status code.
+   *  - Always determine whether an error is fatal or not.. meaning that if it's fatal,
+   *    use log along with a user alert and application reload process (such as a page refresh).
+   */
+
   var debugMode = true;
   console.log('TODO: turn off debug mode in the error handler when in production');
   console.log('TODO: create a better error message for the user when window.onerror executes without debugMode');
@@ -45,6 +72,14 @@ function (Backbone, $, _) {
 
 
   function createError (severity, msg, line) {
+    // causes window.onerror to catch errors instead of sending them to the server
+    if (debugMode && msg instanceof Error) {
+      throw msg;
+    } else if (debugMode && _.isString(msg)) {
+      throw new Error(msg);
+    }
+
+    // start preparing a Backbone Model of the error to send to the server
     var err = new AppError();
 
     if (! _.isNumber(severity)) {
@@ -76,12 +111,14 @@ function (Backbone, $, _) {
     // msg is an app-defined Error type
     if (msg instanceof Error) {
       // the Error 'message' property doesn't show up on object iteration
-      if (msg.message) { err.set('message', msg.message); }
+      if (msg.message) {
+        err.set('message', msg.message);
+      }
 
       // copy each Error property to the Backbone model
       _.each(msg, function (val, key) {
         if (_.has(msg, key)) {
-          err.set([key], val);
+          err.set(key, val);
         }
       });
     }
@@ -89,7 +126,7 @@ function (Backbone, $, _) {
     return sendError(err);
   }
 
-  // handle any errors that crop up during runtime
+  // handle any errors that crop up during runtime and halt execution
   // http://dev.opera.com/articles/view/better-error-handling-with-window-onerror/
   //
   // be aware it doesn't catch everything. Even SyntaxErrors like 'throw,', 'thrownew Error("blah")',
@@ -97,7 +134,8 @@ function (Backbone, $, _) {
   window.onerror = function (msg, url, line) {
     // Allow the browser to handle the error
     if (debugMode) {
-      // returning false means the browser will continue code execution
+      // returning false means the browser will report
+      // the error in console or in an alert to the user
       return false;
 
     // Log the error to the server and continue
@@ -105,7 +143,8 @@ function (Backbone, $, _) {
       createError(1, msg, line);
       $('body').html('This page has encountered an error. Please refresh and try again.');
 
-      // returning true means the browser will halt
+      // returning true means the browser will not report
+      // the error in console or in an alert to the user
       return true;
     }
   };
