1e9a26c842f7d2375ce53f7023115576648d1bbf
David Arvelo <david@davidarvelo.com>
2013-05-22 16:36:43 -0400

    Heavily restructure main app TodosView and its template. Moved saveLayout functionality from user model to TodosView

--- app/scripts/app/models/user.js
+++ app/scripts/app/models/user.js
@@ -15,55 +15,6 @@ function (Backbone, validators, _) {
 
     validate: validators.user,
 
-    saveLayoutSettings: function savePaneSettings () {
-      if (this.app.booting) {
-        return;
-      }
-
-      var self = this,
-          outerLayout = this.app.outerLayout,
-          innerLayout = this.app.innerLayout;
-
-      // overwrite previous to get the latest settings
-      this.tmpUser = {};
-      this.tmpUser.menu = {},
-      this.tmpUser.lastContexts = {},
-      this.tmpUser.paneSizes = {},
-
-      // set menu size and state
-      this.tmpUser.menu.size = outerLayout.west.state.size;
-      this.tmpUser.menu.isClosed = outerLayout.west.state.isClosed;
-
-      // persist old paneSizes
-      this.tmpUser.paneSizes = this.get('paneSizes');
-
-      // write new paneSizes, and overwrite lastContexts and old paneSizes
-      _.each(innerLayout.panes, function ($pane, position) {
-        if ($pane) {
-          this.tmpUser.lastContexts[position] = $pane.data('id');
-
-          // center doesn't have a size
-          if (position !== 'center') {
-            this.tmpUser.paneSizes[position] = innerLayout[position].state.size;
-          }
-        }
-      }, this);
-
-      this.set(this.tmpUser);
-
-      // if not waiting on other model resizes,
-      // kick off waiting process, and when done, save user model.
-      // this assures we get the settings after animations have settled down
-      // or if multiple calls were issued, because a 'closing pane' event also triggers resize events
-      if (!this.waitingForOthers) {
-        this.waitingForOthers = true;
-
-        setTimeout(function () {
-          self.save();
-          self.waitingForOthers = false;
-        }, 600);
-      }
-    },
   });
 
   return User;
--- app/scripts/app/views/todos.js
+++ app/scripts/app/views/todos.js
@@ -4,11 +4,12 @@ define([
   'views/lists/contextPanes',
   'views/lists/contexts',
   'views/config/todosLayout',
+  'views/config/contextsLayout',
   'jquery',
   'underscore',
 ],
 
-function (template, BaseView, ContextPanes, ContextList, outerLayoutConfigGen, $, _) {
+function (template, BaseView, ContextPanes, ContextList, outerLayoutConfigGen, innerLayoutConfigGen, $, _) {
   'use strict';
 
   var TodosView = BaseView.extend({
@@ -17,29 +18,76 @@ function (template, BaseView, ContextPanes, ContextList, outerLayoutConfigGen, $
     template: template,
 
     events: {
-      'paneResize': 'triggerMenuSave',
-      'paneClose': 'triggerMenuSave',
+      'paneResize': 'saveLayoutSettings',
+      'paneClose': 'saveLayoutSettings',
     },
 
-    initialize: function initialize (_app) {
-      console.log('app', _app);
-      this.app = _app;
+    initialize: function initialize (options) {
+      this.app = options.app;
+
+      this.subViews = {};
+
+      // used to store information about which
+      // context occupies which inner layout pane
+      this.contextMap = {};
+
+      this.listenTo(this.app, 'todos:init', this.initLayout);
+      this.listenTo(this.app.collections.contexts, 'reset', this.renderLayout);
+      // might be useful?
+      this.listenTo(this.app, 'todos:saveLayout', this.saveLayoutSettings);
     },
 
     render: function render () {
-      var outerLayoutConfig = outerLayoutConfigGen('.contextsList', '.contextsPanes'),
-          menu = this.app.user.get('menu') || {};
+      this.$el.html(template());
+
+      return this;
+    },
+
+    initLayout: function initLayout () {
+      /*
+       * Render Main Todos Layout and Context Panes Layout
+       */
+      var outerLayoutConfig = outerLayoutConfigGen('outer-pane', '.contextsList', '.contextsPanes');
+      var innerLayoutConfig = innerLayoutConfigGen('inner-pane', '.activateAContext');
+
+      var menu = this.app.user.get('menu') || {};
+      var paneSizes = this.app.user.get('paneSizes') || {};
 
       // set up layout with last user settings if they exist.
       // this needs to be done before initializing the layout with $el.layout(settings)
-      outerLayoutConfig.west__size = menu.size || outerLayoutConfig.west.size;
-      outerLayoutConfig.west__initClosed = menu.isClosed || outerLayoutConfig.west.initClosed;
+      outerLayoutConfig.west.size = menu.size || outerLayoutConfig.west.size;
+      outerLayoutConfig.west.initClosed = menu.isClosed || outerLayoutConfig.west.initClosed;
 
-      this.$el.html(template());
-      this.app.$inactiveContexts = $('#inactiveContexts');
-      this.app.$contextsPanes = $('.contextsPanes');
+      /*
+       * Set up the user's last known pane size settings if they exist.
+       * Init all panes to closed in config. The contexts collection will trigger
+       * the active state on itself which the innerLayout $el will listenTo.
+       */
+      _.each(['north', 'south', 'east', 'west'], function (position) {
+        if (paneSizes[position]) {
+          innerLayoutConfig[position].size = paneSizes[position];
+        }
+      }, this);
+
       this.app.outerLayout = this.$el.layout(outerLayoutConfig);
+      this.app.innerLayout = this.$('.contextsPanes').layout(innerLayoutConfig);
 
+
+      /*
+       * Render ContextsList sidebar contents container
+       */
+      this.subViews['contextsList'] = new ContextList({ app: this.app, collection: this.app.collections.contexts });
+      this.$('.contextsList').append( this.subViews['contextsList'].$el );
+
+
+      return this;
+    },
+
+    renderLayout: function renderLayout () {
+      var outerLayout = this.app.outerLayout;
+      var innerLayout = this.app.innerLayout;
+
+/*    OLD CODE
       var lastContexts = this.app.user.get('lastContexts'),
           renderingLists;
 
@@ -54,7 +102,7 @@ function (template, BaseView, ContextPanes, ContextList, outerLayoutConfigGen, $
         collection: this.app.collections.contexts,
         renderingLists: renderingLists,
       });
-
+*/
       return this;
     },
 
@@ -87,9 +135,64 @@ function (template, BaseView, ContextPanes, ContextList, outerLayoutConfigGen, $
       };
     },
 
-    triggerMenuSave: function triggerMenuSave () {
-      this.app.user.saveLayoutSettings();
-    },
+    // _.debounce will keep the context to the view.
+    // i use it here to prevent multiple saves to the server
+    // in the case of many resize/close events happening in succession
+    saveLayoutSettings: _.debounce(function saveLayoutSettings (e, a , bb, c) {
+
+      /*console.log('resizing..',e, a , bb, c);
+      console.log($(e.target));
+      return;
+      */
+
+      var app = this.app;
+      var outerLayout = this.app.outerLayout;
+      var innerLayout = this.app.innerLayout;
+
+      // .set() only the attrs we need to
+      var tmpUser = {};
+      tmpUser.menu = {},
+      tmpUser.lastContexts = {},
+      tmpUser.paneSizes = {},
+
+      // set menu size and state
+      tmpUser.menu.size = outerLayout.west.state.size;
+      tmpUser.menu.isClosed = outerLayout.west.state.isClosed;
+
+      _.each(['north', 'south', 'east', 'west'], function (position) {
+        if (!innerLayout[position].state.isClosed) { // && this.contextMap[position]) {
+          // get the context id that's inside the open pane using the local model map
+          // tmpUser.lastContexts[position] = this.contextMap[position].get('id');
+        }
+
+        tmpUser.paneSizes[position] = innerLayout[position].state.size;
+      }, this);
+
+      var jqXHR = this.app.user.save(tmpUser);
+
+      // validation failed
+      if (!jqXHR ) {
+        // do something to show the user the error
+        // this.app.user.validationError instanceof errorTypes.ValidationError ( don't necessarily need to check that )
+        return;
+      }
+
+      function ajaxSuccess (data, textStatus, jqXHR) {
+
+      }
+
+      function ajaxFail (jqXHR, textStatus, errorThrown) {
+        app.user.set(app.user.previousAttributes());
+      }
+
+      function ajaxAlways (dataOrjqXHR, textStatus, jqXHROrErrorThrown) {}
+
+      jqXHR
+        .done(ajaxSuccess)
+        .fail(ajaxFail)
+        .always(ajaxAlways);
+
+    }, 600),
   });
 
   return TodosView;
