455a82c21f40cfe926f40350decc9850bbef44c3
David Arvelo <david@davidarvelo.com>
2013-06-12 16:17:52 -0400

    Remove DOM Element util functions in light of lodash having _.isElement

--- app/scripts/app/core/util.js
+++ app/scripts/app/core/util.js
@@ -27,31 +27,9 @@ function ($, _) {
     return $.contains(document.body, el.jquery ? el[0] : el); // just in case :)
   }
 
-  // from: http://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
-  function isADomElementOrNode (obj) {
-    //Returns true if it is a DOM node
-    function isNode(o){
-      return (
-        typeof Node === 'object' ? o instanceof Node :
-        o && typeof o === 'object' && typeof o.nodeType === 'number' && typeof o.nodeName==='string'
-      );
-    }
-
-    //Returns true if it is a DOM element
-    function isElement(o){
-      return (
-        typeof HTMLElement === 'object' ? o instanceof HTMLElement : //DOM2
-        o && typeof o === 'object' && o !== null && o.nodeType === 1 && typeof o.nodeName==='string'
-      );
-    }
-
-    return isNode(obj) || isElement(obj);
-  }
-
   return {
     deleteUserProps: deleteUserProps,
-    isAttachedToDOM: isAttachedToDOM,
-    isADomElementOrNode: isADomElementOrNode,
+    isAttachedToDOM: isAttachedToDOM
   };
 
 });
