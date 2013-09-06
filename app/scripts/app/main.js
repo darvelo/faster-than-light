e3da44cbc3a30fafa12a0f93e7b46bbf2fcbd7bb
David Arvelo <david@davidarvelo.com>
2013-09-06 14:14:32 -0400

    Remove reference to experimental model/collection relationship

--- app/scripts/app/main.js
+++ app/scripts/app/main.js
@@ -1,12 +1,10 @@
-require(['app', 'jquery', 'models/todo/contextTodo'], function (App, $, ct) {
+require(['app', 'jquery'], function (App, $) {
   $(document).ready(function () {
     var app = new App();
 
     window.app = app;
     console.log('TODO: Unset window.app');
     console.log('TODO: Unset window.bootstrap');
-
-    window.b = ct;
   });
 });
 
