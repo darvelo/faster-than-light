b5a23693a13b00c281249416acb78cfc8d000eff
David Arvelo <david@davidarvelo.com>
2013-09-06 12:56:30 -0400

    Remove script to count files

--- count-files.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-find . -type f -name \*.js | grep -v -E "components|mocha|node_modules|dist|vendor|browser/lib|app/templates"
-find . -type f -name \*.js | grep -v -E "components|mocha|node_modules|dist|vendor|browser/lib|app/templates" | wc -l
